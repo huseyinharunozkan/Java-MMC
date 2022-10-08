6d80f61ecde05b2707833956e0209f42501607fe
huseyinharunozkan <huseyinharunozkan@gmail.com>
2022-10-08 18:49:15 +0300

    Constructor

---
+++ Constructor/src/SubClass.java
@@ -0,0 +1,25 @@
+
+public class SubClass {
+
+	private String ad ;
+	private int id;
+	public SubClass(String ad, int id) {
+		this.ad = ad;
+		this.id = id;
+	}
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
