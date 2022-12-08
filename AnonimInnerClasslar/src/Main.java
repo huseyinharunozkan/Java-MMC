1b5d56927501dd3a72d7edd3b32b497cb289d374
huseyinharunozkan <huseyinharunozkan@gmail.com>
2022-12-08 17:23:25 +0300

    Update Main.java

--- AnonimInnerClasslar/src/Main.java
+++ AnonimInnerClasslar/src/Main.java
@@ -22,11 +22,11 @@ public class Main {
 		ogrenci.derseGir();
 		System.out.println("*************************");
 		
-		AOgrenci ogrenci2 = new AOgrenci() {
+		AOgrenci ogrenci2 = new AOgrenci("Harun",319) {
 
 			@Override
 			void kayitYaptir() {
-				System.out.println("Kayýt yapýlýyor..");
+				System.out.println("Kayýt yapýlýyor.."+" Ýsim: "+getIsim()+" Numara: "+getNumara());
 				
 			}
 			
@@ -35,6 +35,31 @@ public class Main {
 		ogrenci2.selamla();
 	}
 	public static abstract class AOgrenci{
+		private String isim;
+		private int numara;
+		
+		public AOgrenci(String isim, int numara) {
+			super();
+			this.isim = isim;
+			this.numara = numara;
+		}
+		
+		public String getIsim() {
+			return isim;
+		}
+
+		public void setIsim(String isim) {
+			this.isim = isim;
+		}
+
+		public int getNumara() {
+			return numara;
+		}
+
+		public void setNumara(int numara) {
+			this.numara = numara;
+		}
+
 		abstract void kayitYaptir();
 		public void selamla() {
 			System.out.println("Selamlar");
