588d2a7ddcb25dcb7fd039d658ced657e1d1abbb
huseyinharunozkan <huseyinharunozkan@gmail.com>
2022-12-25 16:39:23 +0300

    MMC

---
+++ Paketler3/src/com/huseyinharun/matematik/IMatematik.java
@@ -0,0 +1,11 @@
+package com.huseyinharun.matematik;
+
+public interface IMatematik {
+	
+	void toplama(int a, int b);
+	void cikarma(int a, int b);
+	void carpma(int a, int b);
+	void bolme(int a, int b);
+	
+
+}
---
+++ jarDeneme/src/jarDeneme/Main.java
@@ -0,0 +1,19 @@
+package jarDeneme;
+
+import com.huseyinharun.matematik.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Matematik matematik = new Matematik();
+		
+		matematik.toplama(10, 5);
+		
+		matematik.cikarma(10, 5);
+		matematik.carpma(10, 5);
+		matematik.bolme(10, 5);
+
+	}
+
+}
