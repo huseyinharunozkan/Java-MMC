7e70d2dbad71cacaf17074c160ca96aaa345dac5
huseyinharunozkan <huseyinharunozkan@gmail.com>
2023-01-05 19:31:51 +0300

    MMC

---
+++ CollectionFrameworkLinkedList/src/ArrayandLinkedListMain.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayandLinkedListMain {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> linkedlist = new LinkedList<Integer>();
+		ArrayList<Integer> arraylist = new ArrayList<Integer>();
+		
+	//	zamanHesapla("LinkedList", linkedlist);
+		zamanHesapla("ArrayList", arraylist);
+
+	}
+	public static void zamanHesapla(String veriTipi, List<Integer> list) {
+		
+		// Listin sonuna deðer ekleme
+		long baslangic;
+		long bitis;
+		
+		baslangic = System.currentTimeMillis();
+		for(int i = 0; i<1000000;i++) {
+			
+			list.add(0,i);
+		} 
+		bitis = System.currentTimeMillis();
+		System.out.println(veriTipi +" ekleme süresi "+(bitis- baslangic)+" ms");
+	}
+
+}
